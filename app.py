fa4375a052e0a4e661d3a29341d97c8af5cade35
animusbi-admin <guilherme.lombardi@animusbi.com.br>
2025-02-12 17:17:46 -0300

    first app view

---
+++ app.py
@@ -0,0 +1,27 @@
+# import module
+import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+
+
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days']
+)
+
+authenticator.login()
+
+if st.session_state["authentication_status"]:
+    st.write(f'Bem Vindo *{st.session_state["name"]}*')
+    st.title('Página de Sistema')
+    authenticator.logout()
+elif st.session_state["authentication_status"] is False:
+    st.error('Usuário/Senha inválido')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Por Favor, utilize seu usuário e senha!')
